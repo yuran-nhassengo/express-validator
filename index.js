0d7e1648cfc84f0a43315f5fde1cbb39a88713df
yurannhassengo <yurannhassengo@gmail.com>
2024-07-30 10:22:38 -0700

    Add validator email and min 5

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require('express');
+const app = express();
 port = 4000;
+
+
 
 const {body,validationResult} = require('express-validator');
 
 app.use(express.json());
 
+app.post("/user",[body("username").isEmail(),body("password").isLength({min: 5})],(req,res) =>{
+    const erros = validationResult(req);
+    if(!erros.isEmpty()){
+        return res.status(400).json({erros: erros.array()});
 
+    }
+    res.json({msg:"sucesso"});
+});
 
-const app = express();
 
 app.listen(port, () =>{
     console.log(`rodando na porta ${port}`);
