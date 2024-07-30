dd952e1ff7b4b2188fa5c145cbfd4d7c5142c5ba
yurannhassengo <yurannhassengo@gmail.com>
2024-07-30 11:33:35 -0700

    add route validator with message

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ const {body,validationResult} = require('express-validator');
 
 app.use(express.json());
 
+app.post("/validator-with-msg",[body("username").isEmail().withMessage("Não é um email valido"),
+    body("password").isLength({min: 5}).withMessage("A senha deve conter no minimo 5 caracteres")],(req,res) =>{
+    const erros = validationResult(req);
+    if(!erros.isEmpty()){
+        return res.status(400).json({erros: erros.array()});
+
+    }
+    res.json({msg:"sucesso"});
+});
+
 app.post("/user",[body("username").isEmail(),body("password").isLength({min: 5})],(req,res) =>{
     const erros = validationResult(req);
     if(!erros.isEmpty()){
